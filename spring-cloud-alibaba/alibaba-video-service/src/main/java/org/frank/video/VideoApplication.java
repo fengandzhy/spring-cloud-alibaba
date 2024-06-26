136d9c1b7a911f25aeac21bb5e0cf60efa6efd30
fengandzhy <fengandzhy@gmail.com>
2024-06-26 21:43:49 +1200

    update the VideoApplication

--- spring-cloud-alibaba/alibaba-video-service/src/main/java/org/frank/video/VideoApplication.java
+++ spring-cloud-alibaba/alibaba-video-service/src/main/java/org/frank/video/VideoApplication.java
@@ -1,9 +1,13 @@
 package org.frank.video;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("org.frank.video")
+@MapperScan("org.frank.video.repositories")
 public class VideoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VideoApplication.class,args);
+    }
 }
