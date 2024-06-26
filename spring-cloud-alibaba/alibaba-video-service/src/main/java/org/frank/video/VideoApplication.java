88b193a554b60d88696e32d6e03e52efededd24a
fengandzhy <fengandzhy@gmail.com>
2024-06-26 21:40:25 +1200

    VideoApplication

---
+++ spring-cloud-alibaba/alibaba-video-service/src/main/java/org/frank/video/VideoApplication.java
@@ -0,0 +1,9 @@
+package org.frank.video;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("org.frank.video")
+public class VideoApplication {
+}
