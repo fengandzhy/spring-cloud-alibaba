b19377cc6ea9fc5ff266553572a6b944eab11d4e
fengandzhy <fengandzhy@gmail.com>
2024-07-15 22:58:17 +1200

    feigh dependency

--- spring-cloud-alibaba/alibaba-order-service/src/main/java/org/frank/order/controllers/OrderController.java
+++ spring-cloud-alibaba/alibaba-order-service/src/main/java/org/frank/order/controllers/OrderController.java
@@ -3,6 +3,7 @@ package org.frank.order.controllers;
 
 import org.frank.domains.Video;
 import org.frank.domains.VideoOrder;
+import org.frank.order.feign.client.VideoFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -21,18 +22,31 @@ public class OrderController {
 
     private DiscoveryClient discoveryClient;
     
-    @RequestMapping("/find/{id}")
-    public VideoOrder getOrderById(@PathVariable int id){
+    private VideoFeignClient videoFeignClient;
+    
+    @RequestMapping("/find/{videoId}")
+    public VideoOrder getOrderById(@PathVariable int videoId){
         List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("alibaba-video-service");
         ServiceInstance serviceInstance = serviceInstanceList.get(0);
 //        String url = "http://" + serviceInstance.getHost()+":"+serviceInstance.getPort()+"/api/v1/video/"+id;
-        String url = "http://alibaba-video-service/api/v1/video/"+id; // 这里只有用Ribbon 才能用这种方式调用 
-        Video video = restTemplate.getForObject(url, Video.class);
+//        String url = "http://alibaba-video-service/api/v1/video/"+id; // 这里只有用Ribbon 才能用这种方式调用 
+//        Video video = restTemplate.getForObject(url, Video.class);
+//        Video video = videoFeignClient.getById(videoId);
+        Video video = videoFeignClient.getById(videoId);
         VideoOrder order = new VideoOrder();
         order.setVideoId(video.getId());
         order.setVideoTitle(video.getTitle());        
         return order;
     }
+
+//    @RequestMapping("/find")
+//    public VideoOrder findOrderById(int id){        
+//        Video video = videoFeignClient.findById(id);
+//        VideoOrder order = new VideoOrder();
+//        order.setVideoId(video.getId());
+//        order.setVideoTitle(video.getTitle());
+//        return order;
+//    }
 
     @Autowired
     public void setRestTemplate(RestTemplate restTemplate) {
@@ -42,5 +56,10 @@ public class OrderController {
     @Autowired
     public void setDiscoveryClient(DiscoveryClient discoveryClient) {
         this.discoveryClient = discoveryClient;
+    }
+
+    @Autowired
+    public void setVideoFeignClient(VideoFeignClient videoFeignClient) {
+        this.videoFeignClient = videoFeignClient;
     }
 }
