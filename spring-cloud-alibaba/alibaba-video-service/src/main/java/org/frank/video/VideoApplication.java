7789e77957d38490aee1f9a0dc444b84345591ce
fengandzhy <fengandzhy@gmail.com>
2024-07-15 22:18:47 +1200

    feigh dependency

--- spring-cloud-alibaba/alibaba-video-service/src/main/java/org/frank/video/VideoApplication.java
+++ spring-cloud-alibaba/alibaba-video-service/src/main/java/org/frank/video/VideoApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("org.frank.video.repositories")
-@EnableDiscoveryClient
+@EnableDiscoveryClient // 开启服务发现
+@EnableFeignClients  // 开启feign 支持
 public class VideoApplication {
     public static void main(String[] args) {
         SpringApplication.run(VideoApplication.class,args);
