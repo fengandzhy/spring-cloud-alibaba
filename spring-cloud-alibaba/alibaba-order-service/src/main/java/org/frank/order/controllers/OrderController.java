56fb4708be91fbf911bb718912db285126ed3dda
fengandzhy <fengandzhy@gmail.com>
2024-07-16 19:48:03 +1200

    save video

--- spring-cloud-alibaba/alibaba-order-service/src/main/java/org/frank/order/controllers/OrderController.java
+++ spring-cloud-alibaba/alibaba-order-service/src/main/java/org/frank/order/controllers/OrderController.java
@@ -7,10 +7,7 @@ import org.frank.order.feign.client.VideoFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -47,6 +44,11 @@ public class OrderController {
         order.setVideoId(video.getId());
         order.setVideoTitle(video.getTitle());
         return order;
+    }
+
+    @PostMapping("/save")
+    public int saveVideo(@RequestBody Video video){
+        return videoFeignClient.saveVideo(video);
     }
 
     @Autowired
--- spring-cloud-alibaba/alibaba-order-service/src/main/java/org/frank/order/feign/client/VideoFeignClient.java
+++ spring-cloud-alibaba/alibaba-order-service/src/main/java/org/frank/order/feign/client/VideoFeignClient.java
@@ -2,9 +2,7 @@ package org.frank.order.feign.client;
 
 import org.frank.domains.Video;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name="alibaba-video-service")
 public interface VideoFeignClient {
@@ -14,4 +12,7 @@ public interface VideoFeignClient {
 
     @GetMapping(value = "/api/v1/video/find/{videoId}")
     Video getById(@PathVariable("videoId") int videoId);
+
+    @PostMapping(value = "/api/v1/video/save")
+    int saveVideo(@RequestBody Video video);
 }
